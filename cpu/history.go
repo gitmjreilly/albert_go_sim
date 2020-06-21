c30b7617db52bd19d714d65b77c8f2b03223894c
gitmjreilly <gitmjreilly@gmail.com>
2020-06-21 18:56:20 -0400

    More history work

--- cpu/history.go
+++ cpu/history.go
@@ -111,8 +111,6 @@ func createDisassemblyString(s Status) string {
 
 	// f JMPF dst
 	if opCode == branchFalseOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X] JMPF %04X | %s", absoluteAddress, rightOperand, inlineOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X] JMPF %04X", rightOperand, inlineOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -121,7 +119,8 @@ func createDisassemblyString(s Status) string {
 
 	// DI
 	if opCode == diOpcode {
-		disassemblyString := fmt.Sprintf("%08X  DI | %s", absoluteAddress, stackString)
+		instructionString := fmt.Sprintf("DI")
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
@@ -134,7 +133,8 @@ func createDisassemblyString(s Status) string {
 
 	// a DROP
 	if opCode == dropOpcode {
-		disassemblyString := fmt.Sprintf("%08X  [%04X] DROP | %s", absoluteAddress, rightOperand, stackString)
+		instructionString := fmt.Sprintf("[%04X] DROP", rightOperand)
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
@@ -148,7 +148,6 @@ func createDisassemblyString(s Status) string {
 
 	// a b EQUAL
 	if opCode == equallOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] EQUAL | %s", absoluteAddress, leftOperand, rightOperand, stackString)
 		instructionString := fmt.Sprintf("[%04X %04X] EQUAL", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
@@ -156,7 +155,8 @@ func createDisassemblyString(s Status) string {
 
 	// EI
 	if opCode == eiOpcode {
-		disassemblyString := fmt.Sprintf("%08X  EI | %s", absoluteAddress, stackString)
+		instructionString := fmt.Sprintf("EI")
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
@@ -207,8 +207,6 @@ func createDisassemblyString(s Status) string {
 
 	// a b LESS
 	if opCode == lessOpcode || opCode == sLessOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] LESS | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] LESS", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -221,13 +219,10 @@ func createDisassemblyString(s Status) string {
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
 		return disassemblyString
-
 	}
 
 	// a b *
 	if opCode == mulOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] MUL | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] MUL", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -236,20 +231,20 @@ func createDisassemblyString(s Status) string {
 
 	// a NEG?
 	if opCode == negOpcode {
-		disassemblyString := fmt.Sprintf("%08X  [%04X] NEG? | %s", absoluteAddress, rightOperand, stackString)
+		instructionString := fmt.Sprintf("[%04X] NEG?", rightOperand)
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
 	// NOP
 	if opCode == nopOpcode {
-		disassemblyString := fmt.Sprintf("%08X  NOP | %s", absoluteAddress, stackString)
+		instructionString := fmt.Sprintf("NOP")
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
 	// a b OR
 	if opCode == orOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] OR | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] OR", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -258,8 +253,6 @@ func createDisassemblyString(s Status) string {
 
 	// a b +
 	if opCode == plusOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] PLUS | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] PLUS", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -275,16 +268,15 @@ func createDisassemblyString(s Status) string {
 
 	// R_FETCH
 	if opCode == rFetchOpcode {
-		disassemblyString := fmt.Sprintf("%08X  [RTOS: %04X] R_FETCH | %s", absoluteAddress, rtosOperand, stackString)
+		instructionString := fmt.Sprintf("[RTOS: %04X] R_FETCH", rtosOperand)
+		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 		return disassemblyString
 	}
 
 	// SP_FETCH
 	if opCode == spFetchOpcode {
-
 		instructionString := fmt.Sprintf("[PSP: %04X] SP_FETCH", pspOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
-
 		return disassemblyString
 	}
 
@@ -297,8 +289,6 @@ func createDisassemblyString(s Status) string {
 
 	// val addr STORE
 	if opCode == storeOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] STORE | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] STORE", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -314,19 +304,14 @@ func createDisassemblyString(s Status) string {
 
 	// a b -
 	if opCode == subOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] SUB | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] MINUS", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
 		return disassemblyString
-
 	}
 
 	// a b SWAP
 	if opCode == swapOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] SWAP | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] SWAP", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
@@ -350,8 +335,6 @@ func createDisassemblyString(s Status) string {
 
 	// a b XOR
 	if opCode == xorOpcode {
-		// disassemblyString := fmt.Sprintf("%08X  [%04X %04X] XOR | %s", absoluteAddress, leftOperand, rightOperand, stackString)
-
 		instructionString := fmt.Sprintf("[%04X %04X] XOR", leftOperand, rightOperand)
 		disassemblyString := fmt.Sprintf("%08X  %25s | %s", absoluteAddress, instructionString, stackString)
 
